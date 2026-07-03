b3615be331f69627094c782695c690c88ec7c7d0
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-07-03 17:03:08 +0000

    move entire project to the main file for avoiting multi folders

--- .adonisjs/client/registry/index.ts
+++ .adonisjs/client/registry/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import type { AdonisEndpoint } from '@tuyau/core/types'
-import type { Registry } from './schema.d.ts'
-import type { ApiDefinition } from './tree.d.ts'
+import type { Registry } from './schema'
+import type { ApiDefinition } from './tree'
 
 const placeholder: any = {}
 
